b75f2ca169512235e7a494db0e45648033b726fd
stitou <daoumi22@gmail.com>
2017-01-20 11:39:11 +0100

    Lee filtre  v2

--- scriptFilters/lee.py
+++ scriptFilters/lee.py
@@ -5,9 +5,9 @@ from scipy.ndimage.measurements import variance
 import matplotlib.pyplot as plt
 
 
-def lee_filter(img, size):
-    mean = uniform_filter(img, size)
-    pow2_mean = uniform_filter(img**2, size)
+def lee_filter(img, size,modeBorders):
+    mean = uniform_filter(img, size,mode=modeBorders)
+    pow2_mean = uniform_filter(img**2, size,mode=modeBorders)
     mean_variance = pow2_mean - mean**2
     variance_speckle = variance(img)
     img_weights = mean_variance**2 / (mean_variance**2 + variance_speckle**2) # W = O2 / (O2 + o2)
@@ -17,9 +17,10 @@ def lee_filter(img, size):
 if __name__ == '__main__':
     imageLien = str(sys.argv[1])
     sizeMatrix = int(sys.argv[2])
+    modeBorders = str(sys.argv[3])
     nameImage, formatImage = imageLien.split(".")
     uploadedImage = scipy.misc.imread(imageLien)
-    imageFiltred = lee_filter(uploadedImage,sizeMatrix)
+    imageFiltred = lee_filter(uploadedImage,sizeMatrix,modeBorders)
     scipy.misc.imsave(nameImage+'_LeeFilter.%s'%formatImage, imageFiltred)
     plt.hist(imageFiltred.ravel(),histtype='barstacked')
     plt.savefig('histogram.png')
